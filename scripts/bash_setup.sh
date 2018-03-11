a232fd3aeb020c81b869ab1945f97026ab140b37
showlet <showlet95@hotmail.com>
2018-03-11 03:57:31 -0400

    add unicode and lamba symbol

--- scripts/bash_setup.sh
+++ scripts/bash_setup.sh
@@ -1,4 +1,10 @@
-sudo cp ../config/bash/.bash_ps1 ~/
-yes | cp -rf config/bash/.bash_ps1 ~/
-source ~/.bash_ps1
-echo "source ~/.bash_ps1" >> ~/.bashrc 
+# enable utf8 unicode
+sudo locale-gen "en_US.UTF-8";
+sudo dpkg-reconfigure locales
+LC_ALL=en_US.UTF-8
+LANG=en_US.UTF-8
+
+sudo cp ../config/bash/.bash_ps1 ~/;
+yes | cp -rf config/bash/.bash_ps1 ~/;
+echo "source ~/.bash_ps1" >> ~/.bashrc;
+source ~/.bash_ps1;
