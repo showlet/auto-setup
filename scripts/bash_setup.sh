03755c6a544dffee9d5f3c6900edec339eb30819
showlet <showlet95@hotmail.com>
2018-03-11 09:32:51 -0400

    Fix bash_ps1 for real this time.

--- scripts/bash_setup.sh
+++ scripts/bash_setup.sh
@@ -5,6 +5,6 @@ LC_ALL=en_US.UTF-8
 LANG=en_US.UTF-8
 
 sudo cp ../config/bash/.bash_ps1 ~/;
-yes | cp -rf config/bash/.bash_ps1 ~/;
-echo "source ~/.bash_ps1" >> ~/.bashrc;
-source ~/.bash_ps1;
+yes | sudo cp -rf config/bash/.bash_ps1 ~/;
+echo ". ~/.bash_ps1" >> ~/.bashrc;
+. ~/.bash_ps1;
